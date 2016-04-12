d5c818a762ad949e3a63b45643c98414edb1f075
Luke Smith <luke@smith.ac>
2016-04-12 16:10:18 +0100

    Fixes issues with imports and prompts for OS email.

--- bin/linux/gm-download-os.sh
+++ bin/linux/gm-download-os.sh
@@ -6,11 +6,22 @@ echo "-----------------------------------"
 echo "--> Downloading archives..."
 echo "-----------------------------------"
 cd /vagrant/source/os/
+
+if [ ! -e source_email.txt ]; then
+	echo "You need to request an OS OpenData download from the Ordnance Survey website."
+	echo "Once you have done this, press any key, then copy and paste the email."
+	echo ""
+	echo "Press Ctrl+O and return to save the file. Press Ctrl+X to exit again."
+	read -n 1 -s
+	nano -w source_email.txt
+fi
+
 dos2unix source_email.txt
 grep -oP 'Download link\: \K(http://[^\n]+)' source_email.txt | while read -r fileURL ; do
 	fileName=`echo -n "$fileURL" | grep -oP '\/\K([^\/\?]+)' | tail -1 | xargs echo -n`
 	fileDecoded=`echo -n "$fileURL" | python -c "import sys, urllib as ul; sys.stdout.write(ul.unquote(sys.stdin.read()))"`
-	if [ -e $fileName ]
+	
+	if [ $(find ./ -iname ${fileName} | wc -l) -eq 1 ]
 	then
 		echo "Skipping $fileName -- already exists. Delete to redownload."
 	else
@@ -30,5 +41,7 @@ do
 	mkdir ${FileComponents[0]} > /dev/null 2> /dev/null
 	mv "$z" "${FileComponents[0]}/"
 done
+
+cd - > /dev/null
 
 echo "--> Download complete. Run gm-import-os."
